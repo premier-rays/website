03d9a1b1b36f66abc3a7c445d5f586ad46f9d077
Sai Charan <bitsaicharansulibele@gmail.com>
2025-10-18 17:17:17 +0530

    added extra files

--- premier-rays-website/vite.config.js
+++ premier-rays-website/vite.config.js
@@ -15,6 +15,14 @@ export default defineConfig({
         sports: resolve(__dirname, 'sports.html'),
         travels: resolve(__dirname, 'travels.html'),
         tourism: resolve(__dirname, 'tourism.html'),
+        innova: resolve(__dirname, 'innova.html'),
+        urbania: resolve(__dirname, 'urbania.html'),
+        vin: resolve(__dirname, 'vin-academy.html'),
+        accessories: resolve(__dirname, 'accessories.html'),
+        ecostay: resolve(__dirname, 'ecostay.html'),
+        coffeevilla: resolve(__dirname, 'coffeevilla.html'),
+        treks: resolve(__dirname, 'treks.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
       }
     }
   }
